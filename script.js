bcb1f90e90ebb55984cedffd0a135f4d7f7c5ff3
Malvika Sinha <80953969+malvika-sinha@users.noreply.github.com>
2024-06-30 15:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function showVehicles() {
+    alert("Show current vehicles functionality not implemented.");
+}
+
+function showVehicleDetails() {
+    const vehicleDetails = document.getElementById('vehicle-details');
+    vehicleDetails.innerHTML = '';
+
+    const vehicles = [
+        'Vehicle 1', 'Vehicle 2', 'Vehicle 3', 'Vehicle 4', 'Vehicle 5',
+        'Vehicle 6', 'Vehicle 7', 'Vehicle 8', 'Vehicle 9', 'Vehicle 10'
+    ];
+
+    vehicles.forEach(vehicle => {
+        const vehicleItem = document.createElement('div');
+        vehicleItem.className = 'vehicle-item';
+        vehicleItem.innerHTML = `
+            <span>${vehicle}</span>
+            <button onclick="allowVehicle('${vehicle}')">Allow</button>
+            <button onclick="denyVehicle('${vehicle}')">Deny</button>
+            <button onclick="moreDetails('${vehicle}')">More Details</button>
+        `;
+        vehicleDetails.appendChild(vehicleItem);
+    });
+
+    vehicleDetails.classList.remove('hidden');
+}
+
+function allowVehicle(vehicle) {
+    alert(`Allowing ${vehicle}`);
+}
+
+function denyVehicle(vehicle) {
+    alert(`Denying ${vehicle}`);
+}
+
+function moreDetails(vehicle) {
+    alert(`More details for ${vehicle}`);
+}
